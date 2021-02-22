f44d8a2d1cd36688addfcd76aa1ddafc26c287c8
g33krock <dallas.lovell@gmail.com>
2021-02-21 18:25:12 -0700

    post added

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.get('/', express.static('public'))
 
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- queries.js
+++ queries.js
@@ -19,66 +19,56 @@ const getUsers = (request, response) => {
   const getUserById = (request, response) => {
     const id = parseInt(request.params.id)
     
-    pool.connect()
-    .then(() => console.log("Successful connection... you are the best!"))
-    .then(() => pool.query('SELECT * FROM teachers WHERE teacher_id = $1', [id], (error, results) => {
+    console.log("Successful connection... you are the best!")
+    pool.query('SELECT * FROM teachers WHERE teacher_id = $1', [id], (error, results) => {
       if (error) {
         throw error
       }
       response.status(200).json(results.rows)
-    }))
-    .catch(e => console.log)
-    .finally(() => pool.end())
+    })
   }
   
   const createUser = (request, response) => {
-    const { firstname, lastname, campus, role } = request.body
-    
-    pool.connect()
-    .then(() => console.log("Successful connection... you are the best!"))
-    .then(() => pool.query('INSERT INTO users (firstname, lastname, campus, role) VALUES ($1, $2, $3, $4)', [firstname, lastname, campus, role], (error, results) => {
+    const { teacher_id, firstname, lastname, campus, role } = request.body
+  
+    pool.query('INSERT INTO teachers (teacher_id, firstname, lastname, campus, role) VALUES ($1, $2, $3, $4, $5)', [teacher_id, firstname, lastname, campus, role], (error, results) => {
       if (error) {
         throw error
       }
-      response.status(201).send(`User added with ID: ${result.insertId}`)
-    }))
-    .catch(e => console.log)
-    .finally(() => pool.end())
+      response.status(201).send(`User added`)
+    })
   }
   
-  const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { firstname, lastname, campus, role, teacher_id } = request.body;
-  
-    pool.connect()
-    .then(() => console.log("Successful connection... you are the best!"))
-    .then(() => pool.query(
-      'UPDATE users SET firstname = $1, lastname = $2, campus = $3, role = $4 WHERE id = $5',
-      [firstname, lastname, campus, role, teacher_id],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).send(`User modified with ID: ${id}`)
+  function updateUser(request, response) {
+  const id = parseInt(request.params.id);
+  const { firstname, lastname, campus, role, teacher_id } = request.body;
+
+
+  (() => console.log("Successful connection... you are the best!"))(() => pool.query(
+    'UPDATE teachers SET firstname = $1, lastname = $2, campus = $3, role = $4 WHERE id = $5',
+    [firstname, lastname, campus, role, teacher_id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    ))
-    .catch(e => console.log)
-    .finally(() => pool.end())
-  }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  ))(e => console.log);
+}
   
   const deleteUser = (request, response) => {
     const id = parseInt(request.params.id)
   
-    pool.connect()
-    .then(() => console.log("Successful connection... you are the best!"))
-    .then(() => pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    
+    (() => console.log("Successful connection... you are the best!"))
+    (() => pool.query('DELETE FROM teachers WHERE id = $1', [id], (error, results) => {
       if (error) {
         throw error
       }
       response.status(200).send(`User deleted with ID: ${id}`)
     }))
-    .catch(e => console.log)
-    .finally(() => pool.end())
+    (e => console.log)
+    
   }
   
   module.exports = {
