3a04a77d88a79b2b6d97619d4ec15ad4eb413492
g33krock <dallas.lovell@gmail.com>
2021-07-13 13:36:58 -0700

    Added Redux and React-Redux 7

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,14 +1,14 @@
 import { teacherService } from "../services/teacherService";
 import { studentService } from "../services/studentService";
 import { campusService } from "../services/campusService";
-import { useAuth } from '../contexts/Auth'
+// import { useAuth } from '../contexts/Auth'
 
 export const initialState = {
     teachers: teacherService.all,
     students: studentService.all,
     campuses: campusService.all,
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    users: useAuth()
+    // users: useAuth()
 };
 
 export const Reducer = (state = initialState, action) => {
