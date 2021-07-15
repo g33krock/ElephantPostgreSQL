25b320573827ef25c9872e0629e676fc3e40ca90
g33krock <dallas.lovell@gmail.com>
2021-07-15 09:21:49 -0700

    campus filter 2

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -28,7 +28,8 @@ class Main extends Component {
 
     async componentDidMount() {
         await this.setState({teachers:teacherService.all()})
-        await this.setState({teacher:this.state.teachers.find(uTeacher => uTeacher.email === this.props?.userEmail)})
+        const teacher =this.state.teachers.find(uTeacher => uTeacher.email === this.props?.userEmail)
+        await this.setState({teacher})
         await this.setState({campus: this.state.teacher?.campus.id})
         console.log(this.state.campus)
         console.log(this.state.teacher)
