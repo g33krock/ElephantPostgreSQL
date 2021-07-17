6e44569720f77fbd7dda62d57979eb10633afa37
g33krock <dallas.lovell@gmail.com>
2021-07-16 19:07:12 -0700

    Setting Teacher State in Main Component based on User 3

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -33,7 +33,7 @@ class Main extends Component {
     }
 
     setTeachers() {
-        const teacher = () => {this.state.teachers.find(teacher => teacher.email === this.props.userEmail); return teacher} 
+        const teacher = this.state.teachers.find(teacher => teacher.email === this.props.userEmail)
         this.setState({teacher});
         console.log(this.state.teacher)
     }
