4d4188575196d7248ddf79f087fc07bca2e88f1c
g33krock <dallas.lovell@gmail.com>
2021-07-18 15:04:35 -0700

    Allowing Routes based on Role

--- client/src/components/MainComponent.js
+++ client/src/components/MainComponent.js
@@ -25,26 +25,6 @@ class Main extends Component {
             
         };
     }
-
-    // componentDidMount() {
-    //     fetcher(`${baseURL}/teachers`, {
-    //         method: "GET",
-    //         headers: {
-    //           "Content-Type": "application/json",
-    //         },
-    //       })
-    //       .then(response => {
-    //           return response.json();
-    //       })
-    //       .then((teachers) => {
-    //           this.setState({teachers});
-    //       })
-    //       .then(this.setState({teacher: this.state.teachers.find(teacher => teacher?.email === this.props?.userEmail)}))
-    //       .then(console.log(this.state.teacher))
-    // }
-    
-
-    
     
 
     async componentDidMount() {
@@ -59,30 +39,34 @@ class Main extends Component {
         })
     }
 
-    // async setTeachers() {
-    //     const teacher = this.state.teachers.find(teacher => {teacher?.email === this.props.userEmail})
-    //     this.setState({ teacher });
-    //     console.log(this.state.teacher)
-    // }
 
     render() {
+        if (this.state.teacher.role === 3||this.state.teacher.role === 4) {
         return (
             <div>
             
                 <Switch>
-                    <PrivateRoute path='/sped' component={Sped} />
-                    <PrivateRoute path='/schedules' component={Schedule} campusId = {this.state.campus}/>
-                    <PrivateRoute path='/teachers' component={Teacher} />
                     <PrivateRoute path='/singleteachers' component={SingleTeacher} userEmail = {this.props?.userEmail} />
-                    <PrivateRoute path='/students' component={Student} />
-                    <PrivateRoute path='/transcripts' component={Transcript} />
                     <PrivateRoute path='/home' component={Home} />
                     <Redirect to='/home' />
                 </Switch>
             </div>
-            
         )
-           
+        } else {
+            return (
+                <div>
+                    <Switch>
+                        <PrivateRoute path='/sped' component={Sped} />
+                        <PrivateRoute path='/schedules' component={Schedule} campusId = {this.state.campus}/>
+                        <PrivateRoute path='/teachers' component={Teacher} />
+                        <PrivateRoute path='/students' component={Student} />
+                        <PrivateRoute path='/transcripts' component={Transcript} />
+                        <PrivateRoute path='/home' component={Home} />
+                        <Redirect to='/home' />
+                    </Switch>
+                </div>
+            )
+        }  
     }
 }
 
